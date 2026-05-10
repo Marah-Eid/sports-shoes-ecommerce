e600041700f1fcded867505f1bb5bf2b21ad4006
Marah-Eid <maraheid228@gmail.com>
2026-05-10 12:53:31 +0300

    Initial ASP.NET Core project with Identity

---
+++ SportsShoesEcommerce/SportsShoesEcommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SportsShoesEcommerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
